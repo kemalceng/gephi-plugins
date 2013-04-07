2c746366fe516a9d7e270430139f79e6265b65fd
Kemal Beşkardeşler <kemalbeskardesler@192.168.1.5>
2013-04-07 22:37:22 +0300

    Remove unused imports..

--- Cna/src/com/cna/myfirstplugin/MultipartiteLayout.java
+++ Cna/src/com/cna/myfirstplugin/MultipartiteLayout.java
@@ -6,7 +6,6 @@ package com.cna.myfirstplugin;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.gephi.data.attributes.api.AttributeColumn;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.graph.api.HierarchicalGraph;
 import org.gephi.layout.spi.Layout;
@@ -14,7 +13,6 @@ import org.gephi.layout.spi.LayoutBuilder;
 import org.gephi.layout.spi.LayoutProperty;
 import org.gephi.utils.longtask.spi.LongTask;
 import org.gephi.utils.progress.ProgressTicket;
-import org.openide.util.Exceptions;
 
 /**
  *
